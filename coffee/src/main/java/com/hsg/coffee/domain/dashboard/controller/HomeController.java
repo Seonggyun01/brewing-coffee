7c3e7062c68a526353ee59c47f758ca90cb51b85
Seonggyun01 <Seonggyun01@users.noreply.github.com>
2026-05-02 19:02:05 +0900

    feat: 루트 홈 화면 추가
    - 루트 경로를 처리하는 HomeController를 추가
    
    - BrewLog 기본 홈 템플릿과 스타일을 작성
    
    - README의 실행 확인 안내를 현재 상태에 맞게 수정

---
+++ coffee/src/main/java/com/hsg/coffee/domain/dashboard/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.hsg.coffee.domain.dashboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
