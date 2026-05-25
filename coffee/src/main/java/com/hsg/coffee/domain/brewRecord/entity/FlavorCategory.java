427bade76070460e5f0d104206a5423301fe73bd
Seonggyun01 <Seonggyun01@users.noreply.github.com>
2026-05-25 23:52:12 +0900

    feat: 원두 등록 UI와 공통 헤더 개선

--- coffee/src/main/java/com/hsg/coffee/domain/brewRecord/entity/FlavorCategory.java
+++ coffee/src/main/java/com/hsg/coffee/domain/brewRecord/entity/FlavorCategory.java
@@ -1,20 +1,20 @@
 package com.hsg.coffee.domain.brewRecord.entity;
 
 public enum FlavorCategory {
-    FLORAL("Floral", "꽃, 허브, 향긋한 인상", "#C8A2C8"),
-    CITRUS("Citrus", "상큼하고 밝은 산미", "#F6C945"),
-    BERRY("Berry", "베리류의 새콤달콤한 인상", "#9B1B5A"),
-    STONE_FRUIT("Stone Fruit", "복숭아, 살구처럼 부드러운 과일감", "#F4A261"),
-    TROPICAL_FRUIT("Tropical Fruit", "열대과일의 달고 풍성한 인상", "#F9A03F"),
-    APPLE_PEAR("Apple / Pear", "사과, 배처럼 맑고 산뜻한 과일감", "#B7D968"),
-    SWEET("Sweet", "설탕, 꿀, 시럽 같은 단맛", "#D6A04D"),
-    CHOCOLATE("Chocolate", "초콜릿, 코코아 계열의 묵직한 단맛", "#5C3A2E"),
-    NUTTY("Nutty", "견과류의 고소한 인상", "#A47148"),
-    ROASTED_SMOKY("Roasted / Smoky", "로스팅, 스모키, 구운 향", "#3B2F2F"),
-    SPICE("Spice", "향신료의 따뜻하고 자극적인 느낌", "#B5532F"),
-    TEA_LIKE("Tea-like", "차처럼 맑고 우아한 느낌", "#8A9A5B"),
-    WINEY_FERMENTED("Winey / Fermented", "와인, 발효, 숙성된 과일 느낌", "#7B2D43"),
-    EARTHY_HERBAL("Earthy / Herbal", "흙, 허브, 식물성 느낌", "#6B705C");
+    FLORAL("꽃", "꽃, 허브, 향긋한 인상", "#C8A2C8"),
+    CITRUS("시트러스", "상큼하고 밝은 산미", "#F6C945"),
+    BERRY("베리", "베리류의 새콤달콤한 인상", "#9B1B5A"),
+    STONE_FRUIT("핵과류", "복숭아, 살구처럼 부드러운 과일감", "#F4A261"),
+    TROPICAL_FRUIT("열대과일", "열대과일의 달고 풍성한 인상", "#F9A03F"),
+    APPLE_PEAR("사과/배", "사과, 배처럼 맑고 산뜻한 과일감", "#B7D968"),
+    SWEET("단맛", "설탕, 꿀, 시럽 같은 단맛", "#D6A04D"),
+    CHOCOLATE("초콜릿", "초콜릿, 코코아 계열의 묵직한 단맛", "#5C3A2E"),
+    NUTTY("견과류", "견과류의 고소한 인상", "#A47148"),
+    ROASTED_SMOKY("로스티드", "로스팅, 스모키, 구운 향", "#3B2F2F"),
+    SPICE("향신료", "향신료의 따뜻하고 자극적인 느낌", "#B5532F"),
+    TEA_LIKE("차", "차처럼 맑고 우아한 느낌", "#8A9A5B"),
+    WINEY_FERMENTED("발효/와인", "와인, 발효, 숙성된 과일 느낌", "#7B2D43"),
+    EARTHY_HERBAL("흙/허브", "흙, 허브, 식물성 느낌", "#6B705C");
 
     private final String displayName;
     private final String description;
